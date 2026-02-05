2a8c6f17e6b784a29b225769b043e3df8c9197cb
bjoshua <joshua.budgen@ynov.com>
2026-02-05 17:00:04 +0100

    feat(database/constants): Configuration Files - Create constants.php for all files can get config easily from everywhere, add a password mine size and verify with one simple call if user have a session. - database.php crate attribs and try to connect to the database with local and root simple config.

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+// Path for call files from the page HTML without full path (called in the header of each page)
+define('BASE_PATH', dirname(__DIR__));
+define('PUBLIC_PATH', BASE_PATH . '/public');
+
+// Security
+define('PASSWORD_MIN_LENGTH', 8);
+
+// Session to store user data across pages (called in the header of each page)
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
---
+++ config/database.php
@@ -0,0 +1,29 @@
+<?php
+class Database {
+    // Database connection parameters
+    private $host = 'localhost';
+    private $db_name = 'steam_city';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function getConnection() {
+        // Initialize the connection variable
+        $this->conn = null;
+
+        // Attempt to establish a connection to the database
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch(PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
